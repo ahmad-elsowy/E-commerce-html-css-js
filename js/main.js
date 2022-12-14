c7ad97bfcc895e9f9af58f4f5e6683b2054ae4ce
ahmed-elsowy <elsowy2017@gmail.com>
2022-12-14 13:17:17 +0200

    2nd-day

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ let closeNavBar = document.querySelector(".close-nav-bar")
 let navBar  = document.querySelector(".nav-bar")
 let openNavBar  = document.querySelector(".open-nav-bar")
 
-
-addEventListener('click' , (e)=>{
-    navBar.classList.toggle("open") 
-    
-})
-if( navBar.classList.contains("open")){
-    closeNavBar.style.display = "flex"
+if(openNavBar){
+    openNavBar.addEventListener('click' , ()=>{
+        navBar.classList.add("open")
+    })
+}
+if(closeNavBar){
+    closeNavBar.addEventListener('click' , ()=>{
+        navBar.classList.remove("open")
+    })
 }
